85751030a9428da815d29cfb6bbde81e6e5e43ad
Twotiger <dgl0518@gmail.com>
2015-08-31 16:27:52 +0800

    reset

--- qa/mypaginator.py
+++ qa/mypaginator.py
@@ -11,16 +11,14 @@ class EmptyPage(InvalidPage):
     pass
 
 
-
-
 class MyPaginator(object):
     def __init__(self, object_list, per_page):
         self.object_list = object_list
         self.per_page = int(per_page)   # 多少个一分
         self.num_pages = None   # 能拆多少个
-
-
-
+        self.num = None
+        self.number = None
+        self.page_list = None
 
     def validate_number(self, number):
         """
@@ -43,7 +41,7 @@ class MyPaginator(object):
         # 可以分成多少页
         self.num = len(self.object_list)
         num_pages = self.num / self.per_page
-        if self.num % self.per_page > 0 :
+        if self.num % self.per_page > 0:
             self.num_pages = num_pages + 1
         else:
             self.num_pages = num_pages
@@ -58,15 +56,12 @@ class MyPaginator(object):
         self.page_list = self.object_list[bottom: up] # 已拆分的列表
         if self.num_pages < 5:
             # 如果能拆的页面小于5
-            self.page_range = range(1,self.num_pages+1)
+            self.page_range = range(1, self.num_pages+1)
         else:
             if self.number >3:
-                self.page_range = range(self.number-2,min(self.number+3,self.num_pages+1))
+                self.page_range = range(self.number-2, min(self.number+3, self.num_pages+1))
             else:
-                self.page_range = range(1,6)
-
-
-
+                self.page_range = range(1, 6)
 
 
 # if __name__ == '__main__':
@@ -76,4 +71,3 @@ class MyPaginator(object):
 #     print x.num_pages
 #     print "第%s页的列表%s"% (page,x.page_list)
 #     print "分页按钮%s"%x.page_range
-    
