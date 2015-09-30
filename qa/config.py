d30f8b1533e1fe5cb199eaed6fe5db0c6ee71e6b
Twotiger <dgl0518@gmail.com>
2015-09-30 18:08:04 +0800

    2015-9-30
    0.3

--- qa/config.py
+++ qa/config.py
@@ -5,6 +5,13 @@ SECRET_KEY = "IXXeA4-Rzu9RB6nkf687UjQt9YCOp1JpWptm0C0y"
 BUCKET_NAME = "iforj"
 
 # 邮箱支持
+# MAIL_HOST = "smtp.yeah.net"
+# MAIL_USER = ""
+# MAIL_PASS = ""
+# MAIL_POSTFIX = "yeah.net"    # 邮箱的后半段
+# HOSTNAME = "www.iforj.com"  # HOSTNAME
+
+
 MAIL_HOST = "smtp.163.com"
 MAIL_USER = "duangguangmoon"
 MAIL_PASS = "duangguang"
@@ -18,6 +25,7 @@ HOSTNAME = "127.0.0.1"  # HOSTNAME
 # 注册
 EMAIL_SALT = "AF4E%W8-9"    # 随机生成
 
-IMAGE_BASE_PATH = "/static/gavin/Documents/github/Django/iforj/qa/static/img/upload/"
+IMAGE_BASE_PATH = "qa/static/img/upload/"
+# IMAGE_BASE_PATH = "/static/gavin/Documents/github/Django/iforj/qa/static/img/upload/"
 IMAGE_APPLICATION_PATH = "/static/img/upload/"
 DEFAULT_IMAGE_PATH = "/static/img/upload/default.jpg"
